5474274eb11d304048778ad58b54c7fc0a240462
Kosei Moriyama <cou929@gmail.com>
2022-10-31 01:30:35 +0900

    support guidance

--- lib/stability_sdk/client.rb
+++ lib/stability_sdk/client.rb
@@ -1,5 +1,6 @@
 require "grpc"
 require "generation_services_pb"
+require "logger"
 
 module StabilitySDK
   class Client
@@ -36,6 +37,14 @@ module StabilitySDK
         stub_params[kw] = options[kw] if options.has_key?(kw)
       end
 
+      if options.has_key?(:logger)
+        @logger = options[:logger]
+      else
+        logger = Logger.new(STDOUT)
+        logger.level = Logger::WARN
+        @logger = logger
+      end
+
       @stub = Gooseai::GenerationService::Stub.new(host, creds, **stub_params)
     end
 
@@ -51,14 +60,14 @@ module StabilitySDK
       steps = options.has_key?(:steps) ? options[:steps].to_i : DEFAULT_STEPS
       seed = options.has_key?(:seed) ? [options[:seed].to_i] : [rand(4294967295)]
       transform = Gooseai::TransformType.new(
-        diffusion: options.has_key?(:sampler) ? SAMPLER_ALGORITHMS[options[:sampler]] : DEFAULT_SAMPLER_ALGORITHM,
+        diffusion: options.has_key?(:sampler) ? SAMPLER_ALGORITHMS[options[:sampler].to_sym] : DEFAULT_SAMPLER_ALGORITHM,
       )
-      parameters = [Gooseai::StepParameter.new(
+      step_parameter = Gooseai::StepParameter.new(
         scaled_step: 0,
         sampler: Gooseai::SamplerParameters.new(
           cfg_scale: options.has_key?(:cfg_scale) ? options[:cfg_scale].to_f : DEFAULT_CFG_SCALE,
         ),
-      )]
+      )
 
       prompt_param = []
       if prompt != ""
@@ -66,19 +75,63 @@ module StabilitySDK
       end
       if options.has_key?(:init_image)
         prompt_param << init_image_to_prompt(options[:init_image])
-        parameters = [Gooseai::StepParameter.new(
-          scaled_step: 0,
-          sampler: Gooseai::SamplerParameters.new(
-            cfg_scale: options.has_key?(:cfg_scale) ? options[:cfg_scale].to_f : DEFAULT_CFG_SCALE,
-          ),
-          schedule: Gooseai::ScheduleParameters.new(
-            start: options.has_key?(:start_schedule) ? options[:start_schedule].to_f : DEFAULT_START_SCHEDULE,
-            end: options.has_key?(:end_schedule) ? options[:end_schedule].to_f : DEFAULT_END_SCHEDULE,
-          ),
-        )]
+        step_parameter.scaled_step = 0
+        step_parameter.sampler = Gooseai::SamplerParameters.new(
+          cfg_scale: options.has_key?(:cfg_scale) ? options[:cfg_scale].to_f : DEFAULT_CFG_SCALE,
+        )
+        step_parameter.schedule = Gooseai::ScheduleParameters.new(
+          start: options.has_key?(:start_schedule) ? options[:start_schedule].to_f : DEFAULT_START_SCHEDULE,
+          end: options.has_key?(:end_schedule) ? options[:end_schedule].to_f : DEFAULT_END_SCHEDULE,
+        )
       end
       if options.has_key?(:mask_image)
         prompt_param << mask_image_to_prompt(options[:mask_image])
+      end
+
+      # CLIP guidance
+      if options.has_key?(:guidance_preset) && options[:guidance_preset] != "GUIDANCE_PRESET_NONE"
+        step_parameter.sampler = nil
+
+        guidance_prompt =
+          if options.has_key?(:guidance_prompt)
+            Gooseai::Prompt.new(text: options[:guidance_prompt])
+          else
+            Gooseai::Prompt.new(text: prompt)
+          end
+
+        guidance_strength = nil
+        if options.has_key?(:guidance_strength) && options[:guidance_strength] != 0
+          guidance_strength = options[:guidance_strength]
+        end
+
+        models = nil
+        if options.has_key?(:guidance_models)
+          models = options[:guidance_models].map { |m| Gooseai::Model.new(alias: m) }
+        end
+
+        cutouts = nil
+        if options.has_key?(:guidance_cuts)
+          cutouts = Gooseai::CutoutParameters.new(count: options[:guidance_cuts])
+        end
+
+        step_parameter.guidance = Gooseai::GuidanceParameters.new(
+          guidance_preset: Gooseai::GuidancePreset.const_get(options[:guidance_preset].to_sym),
+          instances: [
+            Gooseai::GuidanceInstanceParameters.new(
+              guidance_strength: guidance_strength,
+              models: models,
+              cutouts: cutouts,
+              prompt: guidance_prompt,
+            ),
+          ],
+        )
+
+        if transform.diffusion != Gooseai::DiffusionSampler::SAMPLER_K_DPM_2_ANCESTRAL && transform.diffusion != Gooseai::DiffusionSampler::SAMPLER_K_EULER_ANCESTRAL
+          transform = Gooseai::TransformType.new(
+            diffusion: Gooseai::DiffusionSampler::SAMPLER_K_DPM_2_ANCESTRAL
+          )
+          @logger.warn "CLIP guidance is only supported with ancestral samplers. So override it with SAMPLER_K_DPM_2_ANCESTRAL."
+        end
       end
 
       image_param = Gooseai::ImageParameters.new(
@@ -88,7 +141,7 @@ module StabilitySDK
         steps: steps,
         seed: seed,
         transform: transform,
-        parameters: parameters,
+        parameters: [step_parameter],
       )
 
       req = Gooseai::Request.new(
