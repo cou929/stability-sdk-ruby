aa25324a771454b76dd007bc93343c72eb565d0e
Kosei Moriyama <cou929@gmail.com>
2022-11-17 09:06:12 +0900

    move dashboard api client to stable namespace

--- lib/stability_sdk.rb
+++ lib/stability_sdk.rb
@@ -1,7 +1,7 @@
 require "stability_sdk/version"
 require "stability_sdk/client"
 require "stability_sdk/cli"
-require "stability_sdk/unstable/dashboard_client"
+require "stability_sdk/dashboard_client"
 
 module StabilitySDK
   class Error < StandardError; end
