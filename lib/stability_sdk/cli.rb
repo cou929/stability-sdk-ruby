0695bfa91366fefeda174d2d15dad7aa41cb9445
Kosei Moriyama <cou929@gmail.com>
2023-02-11 00:21:05 +0900

    support artifact_type filter

--- lib/stability_sdk/cli.rb
+++ lib/stability_sdk/cli.rb
@@ -4,11 +4,21 @@ module StabilitySDK
       logger.debug "received answer: #{answer}"
       return if options[:no_store]
 
+      allowed_artifact_types = []
+      if options.has_key?(:artifact_types)
+        allowed_artifact_types = options[:artifact_types].map(&:to_sym)
+      end
+
       answer.artifacts.each_with_index do |artifact, idx|
         filename_base = "#{options[:prefix] || "generation"}-#{answer.request_id}-#{answer.answer_id}-#{idx}"
 
         filename = ""
         contents = ""
+
+        if allowed_artifact_types.size > 0 && !allowed_artifact_types.include?(artifact.type)
+          logger.info "skipping #{artifact.type} due to artifact type filter"
+          next
+        end
 
         case artifact.type
         when :ARTIFACT_IMAGE
